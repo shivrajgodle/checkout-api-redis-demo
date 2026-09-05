ca430a22c2cade65ac90f4bcde798e8e17533fee
shivrajgodle <83442544+shivrajgodle@users.noreply.github.com>
2026-09-05 23:10:19 +0530

    Initial commit

---
+++ src/main/java/com/shivraj/checkout_demo/CheckoutDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shivraj.checkout_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shivraj/checkout_demo/CheckoutDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivraj.checkout_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
